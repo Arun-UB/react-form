f687e143ff1b74a66473f1ecb57587351642aec1
Arun-UB <arun.ub29@gmail.com>
2017-07-13 14:57:15 -0400

    Added some styling

--- src/Form.js
+++ src/Form.js
@@ -28,9 +28,9 @@ class Form extends Component {
 
     //Submit handler for the form
     onSubmit = (e) => {
-        
+
         e.preventDefault();
-        
+
         // Call the validation function
         this.setState({
             error: this.validate(this.state)
@@ -38,8 +38,7 @@ class Form extends Component {
 
         const error = Object
             .values(this.state.error)
-            .every((v) => v===''||v);
-
+            .every((v) => v === '' || v);
 
         // Display results
         if (!error) {
@@ -95,78 +94,81 @@ class Form extends Component {
     }
     render() {
         return (
-            <form onSubmit={this.onSubmit}>
-                <div
-                    className={`form-group ${ !this.state.error.name
-                    ? null
-                    : 'has-danger'}`}>
-                    <label htmlFor="name">Name</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="name"
-                        name="name"
-                        value={this.state.name}
-                        onChange=
-                        {(e) =>this.onChange(e)}
-                        placeholder="First Last"/> {this.state.error.name
-                        ? <div className="form-control-feedback">
-                                First name and last name required</div>
-                        : null}
-                </div>
-                <div
-                    className={`form-group ${ !this.state.error.email
-                    ? null
-                    : 'has-danger'}`}>
-                    <label htmlFor="email">Email</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="email"
-                        name="email"
-                        value={this.state.email}
-                        onChange=
-                        {(e) =>this.onChange(e)}
-                        placeholder="Email"/> {this.state.error.email
-                        ? <div className="form-control-feedback">
-                                Valid email required</div>
-                        : null}
-                </div>
-                <div
-                    className={`form-group ${ !this.state.error.bday
-                    ? null
-                    : 'has-danger'}`}>
-                    <label htmlFor="bday">Birthday</label>
-                    <input
-                        className="form-control"
-                        type="text"
-                        name="bday"
-                        value={this.state.bday}
-                        id="bday"
-                        placeholder="MM/DD/YYYY"
-                        onChange={(e) => this.onChange(e)}/> {this.state.error.bday
-                        ? <div className="form-control-feedback">
-                                Invalid date ,input as MM/DD/YYYY</div>
-                        : null}
-                </div>
-                <div
-                    className={`form-group ${ !this.state.error.password
-                    ? null
-                    : 'has-danger'}`}>
-                    <label htmlFor="password">Password</label>
-                    <input
-                        className="form-control"
-                        type="password"
-                        value={this.state.password}
-                        name="password"
-                        id="password"
-                        onChange={(e) => this.onChange(e)}/> {this.state.error.password
-                        ? <div className="form-control-feedback">
-                                Password should be atleast 6 characters</div>
-                        : null}
-                </div>
-                <button type="submit" className="btn btn-primary">Create Account</button>
-            </form>
+            <div className="row justify-content-center">
+                
+                <form className="col-4" onSubmit={this.onSubmit}>
+                    <div
+                        className={`form-group ${ !this.state.error.name
+                        ? null
+                        : 'has-danger'}`}>
+                        <label htmlFor="name">NAME</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="name"
+                            name="name"
+                            value={this.state.name}
+                            onChange=
+                            {(e) =>this.onChange(e)}
+                            placeholder="First Last"/> {this.state.error.name
+                            ? <div className="form-control-feedback d-block bg-danger rounded error-msg">
+                                    First name and last name required</div>
+                            : null}
+                    </div>
+                    <div
+                        className={`form-group ${ !this.state.error.email
+                        ? null
+                        : 'has-danger'}`}>
+                        <label htmlFor="email">EMAIL</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="email"
+                            name="email"
+                            value={this.state.email}
+                            onChange=
+                            {(e) =>this.onChange(e)}
+                            placeholder="Email"/> {this.state.error.email
+                            ? <div className="form-control-feedback d-block bg-danger rounded error-msg">
+                                    Valid email required</div>
+                            : null}
+                    </div>
+                    <div
+                        className={`form-group ${ !this.state.error.bday
+                        ? null
+                        : 'has-danger'}`}>
+                        <label htmlFor="bday">BIRTHDAY</label>
+                        <input
+                            className="form-control"
+                            type="text"
+                            name="bday"
+                            value={this.state.bday}
+                            id="bday"
+                            placeholder="MM/DD/YYYY"
+                            onChange={(e) => this.onChange(e)}/> {this.state.error.bday
+                            ? <div className="form-control-feedback d-block bg-danger rounded error-msg">
+                                    Invalid date ,input as MM/DD/YYYY</div>
+                            : null}
+                    </div>
+                    <div
+                        className={`form-group ${ !this.state.error.password
+                        ? null
+                        : 'has-danger'}`}>
+                        <label htmlFor="password">PASSWORD</label>
+                        <input
+                            className="form-control"
+                            type="password"
+                            value={this.state.password}
+                            name="password"
+                            id="password"
+                            onChange={(e) => this.onChange(e)}/> {this.state.error.password
+                            ? <div className="form-control-feedback d-block bg-danger rounded error-msg">
+                                    Password should be atleast 6 characters</div>
+                            : null}
+                    </div>
+                    <button type="submit" className="btn btn-primary">Create Account</button>
+                </form>
+            </div>
         )
     }
 }
