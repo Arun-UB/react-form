a0fffddf382e0b4aa749166ac176111a13cf4215
Arun-UB <arun.ub29@gmail.com>
2017-07-13 12:03:07 -0400

    Adding event handlers for the form

--- src/Form.js
+++ src/Form.js
@@ -1,23 +1,56 @@
 import React, {Component} from 'react';
 
 class Form extends Component {
+
+    constructor(props) {
+        super();
+
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
+
+        this.state = {
+            name:'',
+            email:''
+        }
+    }
+
+    //Submit handler for the form
+    onSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    //OnChange handler for the form
+    onChange = (e) => {
+        this.setState({
+        [e.target.name] :e.target.value
+        })
+    }
+    
     render() {
         return (
-            <form>
+            <form onSubmit={this.onSubmit}>
                 <div className="form-group">
-                    <label htmlFor="formGroupExampleInput">Example label</label>
+                    <label htmlFor="name">Name</label>
                     <input
                         type="text"
                         className="form-control"
-                        id="formGroupExampleInput"
-                        placeholder="Example input"/></div>
+                        id="name"
+                        name="name"
+                        value={this.state.name}
+                        required
+                        onChange = {(e) =>this.onChange(e)}
+                        placeholder="First Last"/></div>
                 <div className="form-group">
-                    <label htmlFor="formGroupExampleInput2">Another label</label>
+                    <label htmlFor="email">Email</label>
                     <input
                         type="text"
                         className="form-control"
-                        id="formGroupExampleInput2"
-                        placeholder="Another input"/></div>
+                        id="email"
+                        name="email"
+                        required
+                        placeholder="Email"/></div>
+                    
+                <button type="submit" className="btn btn-primary">Create Account</button>
             </form>
         )
     }
